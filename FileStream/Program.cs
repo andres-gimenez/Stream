3c27cd950c338706a76d83ca91f1606378645c7c
Andres Gimenez Muñoz <andres.gimenez@outlook.com>
2021-10-25 18:58:42 +0200

    Add project files.

---
+++ FileStream/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows;
+using System.IO;
+
+namespace FileStreamDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string StartDirectory = @".\FicherosPruebas";
+            string EndDirectory = @".\FicherosPruebas";
+
+            foreach (string filename in Directory.EnumerateFiles(StartDirectory))
+            {
+                using FileStream SourceStream = File.Open(filename, FileMode.Open);
+
+                string nombreCopia = Path.GetFileName(filename) + ".Copia" + Path.GetExtension(filename);
+
+                using FileStream DestinationStream = File.Create(Path.Combine(EndDirectory, nombreCopia));
+                SourceStream.CopyTo(DestinationStream);
+            }
+        }
+    }
+}
