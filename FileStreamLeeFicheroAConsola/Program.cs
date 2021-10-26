5ae91ee029e980978f4acad860643707749e482e
Andres Gimenez Muñoz <andres.gimenez@outlook.com>
2021-10-26 13:56:23 +0200

    Cambios.

--- FileStreamLeeFicheroAConsola/Program.cs
+++ FileStreamLeeFicheroAConsola/Program.cs
@@ -2,6 +2,7 @@
 using System.Threading.Tasks;
 using System.Windows;
 using System.IO;
+using System.Text;
 
 namespace FileStreamLee
 {
@@ -21,7 +22,27 @@ namespace FileStreamLee
 
             var ouputStream = Console.OpenStandardOutput();
 
-            sourceStream.CopyTo(ouputStream);
+            //sourceStream.CopyTo(ouputStream);
+
+            byte[] textSalto = Encoding.UTF8.GetBytes("\n");
+            byte[] textH = Encoding.UTF8.GetBytes("H");
+            byte[] textAsBytes = Encoding.UTF8.GetBytes("Hola mundo Ҟ");
+            byte[] textAsBytes2 = Encoding.Unicode.GetBytes("Hola mundo Ҟ");
+            
+            ouputStream.Write(textAsBytes);
+            ouputStream.WriteByte(10);
+            ouputStream.Write(textAsBytes2);
+            //ouputStream.Write("\n");
+
+            //ouputStream.Write(new byte[] { 72, 111, 108});
+            //ouputStream.WriteByte(111);
+            //ouputStream.WriteByte(108);
+
+            /*for (byte i = 30; i < 255; i++)
+            {
+                ouputStream.WriteByte(i);
+            }*/
+            //            sourceStream.Flush();*/
         }
 
         static void Ejemplo2()
